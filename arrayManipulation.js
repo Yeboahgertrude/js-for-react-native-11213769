62f0bdea31a039b263c250e1792ff1a649810467
Yeboah Gertrude Asabea <gayeboah016@st.ug.edu.gh>
2024-05-19 00:17:56 -0700

    task 3

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -4,8 +4,22 @@ function processArray(arr){
             return num ** 2 
         } 
         else{
-            return num** 3;
+            return num ** 3;
         }
     });
      
+}
+
+function formatArrayStrings(string,number){
+    return strings.map (str, index =>{
+         if (string.length !== numbers.length ){ 
+    return "Error: Array length must be the same.";
+         }
+    if (num[index] %2 === 0){
+    return str.toUpperCase();
+    }
+    else{
+    return str.toLowerCase();
+    }
+    }); 
 }
